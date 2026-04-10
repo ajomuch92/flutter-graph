cafc918f4d0ea6004a28bd2fa6b78ade6e62efc7
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2026-04-09 18:12:33 -0600

    feat: update astro.config.mjs and global.css for vue integration and vis-network styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   }
